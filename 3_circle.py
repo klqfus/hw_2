bbfa1de34f23d6c25c4a9adc210864a6ed8d0f63
Матвей В. Лавров <lavrov_mv@edu.kvantorium-pk.ru>
2020-02-03 18:52:07 +0300

    done

---
+++ 3_circle.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.shape('turtle')
+n = 36
+a = (n-2) * 180 / n
+turtle.left(90=l)
+for i in range(n):
+    turtle.forward(30)
+    turtle.left(n)
---
+++ 4_nested_rectangle.py
@@ -0,0 +1,11 @@
+import turtle
+from rectangle import rectangle
+
+def nested_rectangle(n,l)
+    for i in range(n)
+        rectangle(l*i)
+    pass
+
+
+if __name__ == "__main__":
+    nested_rectangle(10, 10)
